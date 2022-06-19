c98c01debc34f0c5258e5ac46f740700f305e115
Андреев Антон <obsos32@gmail.com>
2022-06-19 18:40:45 +0300

    Групповой проект спринта 10: YaMDb (v. 1.6) - исправление замечаний 1-го ревью

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -137,8 +137,8 @@ class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = [IsAuthorOrIsStaff]
 
-    def get_field(self, model, field):
-        return get_object_or_404(model, id=self.kwargs.get(f'{field}_id'))
+    def get_review(self):
+        return get_object_or_404(Review, id=self.kwargs.get('review_id'))
 
     def get_queryset(self):
         review_id = self.kwargs.get("review_id")
@@ -147,6 +147,4 @@ class CommentViewSet(viewsets.ModelViewSet):
         return Comment.objects.filter(review=review)
 
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user,
-                        title=self.get_field(Title, 'title'),
-                        review=self.get_field(Review, 'review'))
+        serializer.save(author=self.request.user, review=self.get_review())
